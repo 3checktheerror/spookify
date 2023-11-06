7b1070d3bf46069cccfc70bb72526a78a5a3b13d
Qiu Junyi <SWE2109562@xmu.edu.my>
2023-11-06 15:31:28 +0800

    修改监听器+配置PageHelper

--- Assignment/common-infrastructure/src/main/java/com/xmum/swe/listener/SpookifyListener.java
+++ Assignment/common-infrastructure/src/main/java/com/xmum/swe/listener/SpookifyListener.java
@@ -1,18 +1,22 @@
 package com.xmum.swe.listener;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class SpookifyListener implements ApplicationListener<SpooifyEvent> {
+public class SpookifyListener implements ApplicationListener<ApplicationEvent> {
     @Async
     @Override
-    public void onApplicationEvent(SpooifyEvent event) {
+    public void onApplicationEvent(ApplicationEvent event) {
+
         log.info("---SpookifyListener starts working---");
-        SpookifyEventData eventData = event.getEventData();
-        eventData.run();
+        if(event instanceof SpooifyEvent){  //to ensure other events are not captured
+            SpookifyEventData eventData = ((SpooifyEvent) event).getEventData();
+            eventData.run();
+        }
     }
 }
