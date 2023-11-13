fb8a9513c7b8e99862e2638ea6c52267a0926025
Qiu Junyi <SWE2109562@xmu.edu.my>
2023-11-13 21:17:11 +0800

    业务层解耦

--- Assignment/portfolio-provider/src/main/java/com/xmum/swe/service/ItemService.java
+++ Assignment/portfolio-provider/src/main/java/com/xmum/swe/service/ItemService.java
@@ -1,76 +1,25 @@
 package com.xmum.swe.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.xmum.swe.dao.ItemDao;
-import com.xmum.swe.entities.BO.ItemBO;
+
 import com.xmum.swe.entities.DO.ItemDO;
-import com.xmum.swe.exception.SpookifyBusinessException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
-import javax.sql.rowset.serial.SerialException;
-import java.sql.Blob;
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
-@Service
-@Slf4j
-public class ItemService {
-    @Resource
-    private ItemDao itemDao;
 
-    public ItemDO getItemById(String id) {
-        ItemDO item = itemDao.selectById(id);
-        Optional.ofNullable(item)
-                .orElseThrow(() -> new SpookifyBusinessException("No such item!"));
-        return item;
-    }
 
-    public List<ItemDO> getAllItems() {
-        List<ItemDO> items = itemDao.selectList(null);
-        Optional.ofNullable(items)
-                .orElseThrow(() -> new SpookifyBusinessException("items list is empty!"));
-        return items;
-    }
+public interface ItemService {
 
-    public boolean containsItemName(String name) {
-        QueryWrapper<ItemDO> wrapper = new QueryWrapper<>();
-        ItemDO[] items = itemDao
-                .selectList(wrapper.select("name")
-                        .and(i -> i.eq("name", name)))
-                .toArray(new ItemDO[0]);
-        return items.length > 0;
-    }
+    ItemDO getItemById(String id);
 
-    public Map<String, Object> insertItem(ItemDO itemDO) {
-        int num = itemDao.insert(itemDO);
-        Map<String, Object> map = new HashMap<>();
-        map.put("num", num);
-        map.put("id", itemDO.getIId());
-        return map;
-    }
+    List<ItemDO> getAllItems();
 
-    public ItemDO getItemWithMaxId(){
-        return (ItemDO)itemDao.selectList(new QueryWrapper<ItemDO>().orderByDesc("i_id"))
-                .stream().
-                limit(1)
-                .toArray()[0];
-    }
+    boolean containsItemName(String name);
 
-    public Map<String, Object> updateItemById(ItemDO itemDO) {
-        int num = itemDao.updateById(itemDO);
-        Map<String, Object> map = new HashMap<>();
-        map.put("num", num);
-        map.put("id", itemDO.getIId());
-        return map;
-    }
+    Map<String, Object> insertItem(ItemDO itemDO);
 
-    public int deleteItemWithId(String id) {
-        int num = itemDao.deleteById(id);
-        return num;
-    }
+    ItemDO getItemWithMaxId();
+
+    Map<String, Object> updateItemById(ItemDO itemDO);
+
+    int deleteItemWithId(String id);
 }
---
+++ Assignment/portfolio-provider/src/main/java/com/xmum/swe/service/impl/ItemServiceImpl.java
@@ -0,0 +1,73 @@
+package com.xmum.swe.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.xmum.swe.dao.ItemDao;
+import com.xmum.swe.entities.DO.ItemDO;
+import com.xmum.swe.exception.SpookifyBusinessException;
+import com.xmum.swe.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class ItemServiceImpl implements ItemService {
+    @Resource
+    private ItemDao itemDao;
+
+    public ItemDO getItemById(String id) {
+        ItemDO item = itemDao.selectById(id);
+        Optional.ofNullable(item)
+                .orElseThrow(() -> new SpookifyBusinessException("No such item!"));
+        return item;
+    }
+
+    public List<ItemDO> getAllItems() {
+        List<ItemDO> items = itemDao.selectList(null);
+        Optional.ofNullable(items)
+                .orElseThrow(() -> new SpookifyBusinessException("items list is empty!"));
+        return items;
+    }
+
+    public boolean containsItemName(String name) {
+        QueryWrapper<ItemDO> wrapper = new QueryWrapper<>();
+        ItemDO[] items = itemDao
+                .selectList(wrapper.select("name")
+                        .and(i -> i.eq("name", name)))
+                .toArray(new ItemDO[0]);
+        return items.length > 0;
+    }
+
+    public Map<String, Object> insertItem(ItemDO itemDO) {
+        int num = itemDao.insert(itemDO);
+        Map<String, Object> map = new HashMap<>();
+        map.put("num", num);
+        map.put("id", itemDO.getIId());
+        return map;
+    }
+
+    public ItemDO getItemWithMaxId(){
+        return (ItemDO)itemDao.selectList(new QueryWrapper<ItemDO>().orderByDesc("i_id"))
+                .stream().
+                limit(1)
+                .toArray()[0];
+    }
+
+    public Map<String, Object> updateItemById(ItemDO itemDO) {
+        int num = itemDao.updateById(itemDO);
+        Map<String, Object> map = new HashMap<>();
+        map.put("num", num);
+        map.put("id", itemDO.getIId());
+        return map;
+    }
+
+    public int deleteItemWithId(String id) {
+        int num = itemDao.deleteById(id);
+        return num;
+    }
+}
