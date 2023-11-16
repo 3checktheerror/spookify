e57599065cfcaac999845879a86a66d7ede545cc
Xu Zixiang <SWE2109568@xmu.edu.my>
2023-11-16 11:00:24 +0800

    contactMe

--- Assignment/portfolio-provider/src/main/java/com/xmum/swe/controller/Testcontroller.java
+++ Assignment/portfolio-provider/src/main/java/com/xmum/swe/controller/Testcontroller.java
@@ -2,6 +2,7 @@ package com.xmum.swe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,7 +13,15 @@ public class Testcontroller {
     @GetMapping("b")
     public ModelAndView helloWorld() {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("/manageForm");
+        mv.setViewName("/Index");
+        return mv;
+    }
+    @GetMapping("{page}")
+    public ModelAndView accessPage(@PathVariable("page") String page)  {
+        ModelAndView mv = new ModelAndView();
+        StringBuilder sb = new StringBuilder();
+        sb.append("/").append(page);
+        mv.setViewName(sb.toString());
         return mv;
     }
 }
