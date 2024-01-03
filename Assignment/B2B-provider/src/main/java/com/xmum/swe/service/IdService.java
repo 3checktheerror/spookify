465ed3251e320725637795b0ab161c54328ef776
Qiu Junyi <95988378+3checktheerror@users.noreply.github.com>
2024-01-03 22:39:16 +0800

    Revert "Dev hotfix luo"

--- Assignment/B2B-provider/src/main/java/com/xmum/swe/service/IdService.java
+++
@@ -1,5 +0,0 @@
-package com.xmum.swe.service;
-
-public interface IdService {
-    String getNextId(String curMaxId);
-}
--- Assignment/B2B-provider/src/main/java/com/xmum/swe/service/SaveService.java
+++
@@ -1,7 +0,0 @@
-package com.xmum.swe.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.xmum.swe.entities.DO.Save;
-
-public interface SaveService extends IService<Save> {
-}
